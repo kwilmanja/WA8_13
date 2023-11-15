c82767c37811f1f848c4e1ec111af80bd95788c5
Joseph Kwilman <winnie.joe123@gmail.com>
2023-11-14 19:12:25 -0500

    Initial Commit

---
+++ WA8_13UITests/WA8_13UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WA8_13UITestsLaunchTests.swift
+//  WA8_13UITests
+//
+//  Created by Joph Kwilman on 11/14/23.
+//
+
+import XCTest
+
+class WA8_13UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
